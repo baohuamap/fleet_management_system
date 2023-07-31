0c3fea4db36d4b3be7c5f2126d0226d20fdb8da3
bhmap <baohua3110@gmail.com>
2023-07-31 13:55:34 +0700

    31-7

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+from app import models
 from app.config import settings
 from app.database import Base, get_db
 from app.main import app
@@ -25,7 +26,7 @@ TestingSessionLocal = sessionmaker(
 
 
 # check lai sau moi session co drop db k
-@pytest.fixture()
+@pytest.fixture
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -36,7 +37,7 @@ def session():
         db.close()
 
 
-@pytest.fixture()
+@pytest.fixture
 def client(session):
     # run code before run test
     def override_get_db():
@@ -49,3 +50,35 @@ def client(session):
 
     yield TestClient(app)
     # run code after test finishes
+
+
+@pytest.fixture
+def test_fleets(session):
+    fleets_data = [
+        {
+            "fleet_name": "Team A",
+            "fleet_info": "NYC",
+            "phone_number": "113",
+        },
+        {
+            "fleet_name": "Team B",
+            "fleet_info": "BKK",
+            "phone_number": "114",
+        },
+        {
+            "fleet_name": "Team C",
+            "fleet_info": "HCM",
+            "phone_number": "115",
+        },
+    ]
+
+    def create_fleet_model(fleet):
+        return models.Fleet(**fleet)
+
+    fleets = list(map(create_fleet_model, fleets_data))
+
+    session.add_all(fleets)
+    session.commit()
+
+    fleets = session.query(models.Fleet).all()
+    return fleets
--- test/test_fleets.py
+++ test/test_fleets.py
@@ -1,40 +1,65 @@
 # test_fleets.py
 
-from test.database import client, session
-
 import pytest
 
 from app import schemas
 
+# @pytest.mark.parametrize(
+#     "fleet_name, fleet_info, phone_number, expected",
+#     [
+#         ("Team F", "NYC", "113", 201),
+#         ("Team ABC", "NYC", "113", 201),
+#         ("Team bruh bruh", "BKK", "114", 201),
+#         ("Team lmao", "London", "115", 201),
+#     ],
+# )
+# def test_create_fleet(client, fleet_name, fleet_info, phone_number, expected):
+#     ret = client.post(
+#         "/api/fleets/",
+#         json={
+#             "fleet_name": f"{fleet_name}",
+#             "fleet_info": f"{fleet_info}",
+#             "phone_number": f"{phone_number}",
+#         },
+#     )
+#     print(ret.json())
+#     assert ret.status_code == expected
+#     new_fleet = schemas.CreateFleet(**ret.json())
+#     assert new_fleet.fleet_name == fleet_name
+#     assert new_fleet.fleet_info == fleet_info
+#     assert new_fleet.phone_number == phone_number
 
-@pytest.mark.parametrize(
-    "fleet_name, fleet_info, phone_number, expected",
-    [
-        ("Team F", "NYC", "113", 201),
-        ("Team ABC", "NYC", "113", 400),
-        ("Team bruh bruh", "BKK", "114", 201),
-        ("Team lmao", "London", "115", 201),
-    ],
-)
-def test_create_fleet(client, fleet_name, fleet_info, phone_number, expected):
-    ret = client.post(
+
+def test_create_error_fleet(client):
+    ret1 = client.post(
         "/api/fleets/",
         json={
-            "fleet_name": f"{fleet_name}",
-            "fleet_info": f"{fleet_info}",
-            "phone_number": f"{phone_number}",
+            "fleet_name": "a",
+            "fleet_info": "b",
+            "phone_number": "123",
         },
     )
-    print(ret.json())
-    assert ret.status_code == expected
-    if expected == 201:
-        new_fleet = schemas.CreateFleet(**ret.json())
-        assert new_fleet.fleet_name == fleet_name
-        assert new_fleet.fleet_info == fleet_info
-        assert new_fleet.phone_number == phone_number
-    if expected == 400:
-        assert ret.json().get("detail") == "Error create fleet"
+    assert ret1.status_code == 201
+    ret2 = client.post(
+        "/api/fleets/",
+        json={
+            "fleet_name": "c",
+            "fleet_info": "d",
+            "phone_number": "123",
+        },
+    )
+    assert ret2.status_code == 400
+    assert ret2.json().get("detail") == "Error create fleet"
 
 
-def test_get_fleets():
-    pass
+def test_get_fleets(client, test_fleets):
+    ret = client.get("/api/fleets/")
+
+    def validate(fleets):
+        return schemas.Fleet(**fleets)
+
+    fleets_map = map(validate, ret.json())
+    fleets_list = list(fleets_map)
+
+    assert len(fleets_list) == len(test_fleets)
+    assert ret.status_code == 200
